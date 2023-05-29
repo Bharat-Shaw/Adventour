3fa514347dd0859cd9af141cc93f038746fccd4f
iamFaizee <khanfaizan1123@gmail.com>
2023-05-29 11:40:04 +0530

    HomePage is completed by Faizan

--- adventour-app/src/App.js
+++ adventour-app/src/App.js
@@ -2,11 +2,13 @@
 import AllRoutes from './AllRoutes/AllRoutes';
 import Tempnav from './AllRoutes/Tempnav';
 import './App.css';
+import Navbar from './Components/Faizan/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Tempnav/>
+      {/* <Tempnav/> */}
+      <Navbar />
       <AllRoutes/>
     </div>
   );
