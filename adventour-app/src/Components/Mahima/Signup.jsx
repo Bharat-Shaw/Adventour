e6577fef88cfef9d13b9d234d78a028f64302b16
VivekGhalout <vivek.ghalout@gmail.com>
2023-06-01 22:03:23 +0530

    abc

--- adventour-app/src/Components/Mahima/Signup.jsx
+++ adventour-app/src/Components/Mahima/Signup.jsx
@@ -1,112 +1,142 @@
 import React from 'react'
-import { useState, useReducer } from 'react'
-import { Box, Heading, Center, FormControl, Stack, Input, Checkbox, Button, Text, HStack } from '@chakra-ui/react'
+import { useState } from 'react'
+import {
+  Box, Heading, Center, FormControl, Stack,
+  Input, Checkbox, Button, Text, HStack, InputGroup,
+  InputRightElement, IconButton,
+} from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
-import { FcGoogle} from 'react-icons/fc';
-import { Switch } from '@chakra-ui/react'
+import { FcGoogle } from 'react-icons/fc';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 import { useSelector } from 'react-redux';
-
-const initialState = {
-  name: '',
-  email: '',
-  password: '',
-  repeatPassword: '',
-  termsAccepted: false
-};
-
-function reducer(state, action) {
-  return { ...state, [action.input]: action.value };
-}
-
-function validateState(state) {
-  return state.password === state.cpassword
-    && state.termsAccepted
-    && state.password.length > 3;
-}
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { auth } from './firebase';
 
 function Signup() {
-
-  const theme=useSelector(state=>state.theme);
-
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [show, setShow] = React.useState(false)
+  const handleClick = () => setShow(!show)
+  const theme = useSelector(state => state.theme);
+  const [errMsg, setErrMsg] = useState("");
+  const [chkBox, setChkBox] = useState(false);
+  const [subBtnDisable, setSubBtnDisable] = useState(false);
   const navigate = useNavigate();
 
-  console.log(state);
+  // console.log(chkBox)
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    alert(`Hey ${state.name} you have successfully Registered!`);
-    navigate("/login")
-  }
-  const handleInput = (e) => {
-    const { name, value, checked } = e.target
-    const action = {
-      input: name,
-      value: name === "termsAccepted" ? checked : value,
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    conPassword: ""
+  });
+
+  const handleSignUp = () => {
+    if (!userData.name || !userData.email || !userData.password || !userData.conPassword) {
+      setErrMsg("Please fill all the details !");
+      return;
     }
-    dispatch(action);
+    if (userData.password !== userData.conPassword) {
+      setErrMsg("Password not matched !");
+      return;
+    }
+    setErrMsg("");
+
+    setSubBtnDisable(true);
+
+    createUserWithEmailAndPassword(auth, userData.email, userData.password)
+      .then(async (res) => {
+        setSubBtnDisable(false)
+        console.log(res)
+        const user = res.user
+        await updateProfile(user, {
+          displayName: userData.name
+        });
+        navigate("/")
+      })
+      .catch((err) => {
+        setSubBtnDisable(false)
+        setErrMsg(err.message)
+      })
   }
+
+  // console.log(userData)
+
   return (
     <>
-    <Box minH={'100vh'} p='30px 0' bg= {theme ? '#101214' : 'gray.100' } >
-      <Center>
-        <Box borderRadius='20px'  bg= {theme ? '#191b1d' : 'white' } my='20px' p='30px' color={theme ? 'white' : 'black'}>
+      <Box minH={'100vh'} p='30px 0' bg={theme ? '#101214' : 'gray.100'} >
+        <Center>
+          <Box w={{ base: "90%", md: "60%", lg: "28%" }} borderRadius='20px' bg={theme ? '#191b1d' : 'white'} my='20px' p='30px' color={theme ? 'white' : 'black'}>
 
-          <Box mt='3' mb='25' ><Heading><Center>Sign up</Center></Heading></Box>
-          <Stack spacing={3}>
+            <Box mt='3' mb='25' ><Heading><Center>Sign up</Center></Heading></Box>
+            <Stack spacing={3}>
 
-            <FormControl>
-              <Input placeholder='Full Name' name="name" type="text" onChange={handleInput} variant='flushed'  colorScheme='none' />
-            </FormControl>
-            <FormControl>
-              <Input placeholder='Enter your email' size='md' name="email" type="text" onChange={handleInput} variant='flushed' colorScheme='none' />
+              <FormControl>
+                <Input onChange={(e) => { setUserData({ ...userData, name: e.target.value }) }} placeholder='Full Name' name="name" type="text" variant='flushed' colorScheme='none' />
+              </FormControl>
 
-            </FormControl>
-            <FormControl>
-              <Input placeholder='Enter password' size='md' name="password" type="text" onChange={handleInput} variant='flushed' colorScheme='none' />
-            </FormControl>
-            <FormControl>
-              <Input placeholder='Confirm password' size='md' name="cpassword" type="text" onChange={handleInput} variant='flushed' colorScheme='none' />
-            </FormControl>
+              <FormControl>
+                <Input onChange={(e) => { setUserData({ ...userData, email: e.target.value }) }} placeholder='Enter your email' size='md' name="email" type="text" variant='flushed' colorScheme='none' />
+              </FormControl>
 
-          </Stack>
-          <Box width='50vh' m='auto'>
+              <FormControl>
+                <InputGroup>
+                  <Input type={show ? 'text' : 'password'} onChange={(e) => { setUserData({ ...userData, password: e.target.value }) }} placeholder='Enter password' size='md' name="password" variant='flushed' colorScheme='none' />
+                  <InputRightElement width='4.5rem'>
+                    <IconButton
+                      colorScheme='none'
+                      onClick={handleClick}
+                      icon={show ? <ViewOffIcon color={theme ? "white" : "black"} /> : <ViewIcon color={theme ? "white" : "black"} />}
+                    />
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+
+              <FormControl>
+                <InputGroup>
+                  <Input type={show ? 'text' : 'password'} onChange={(e) => { setUserData({ ...userData, conPassword: e.target.value }) }} placeholder='Confirm password' size='md' name="cpassword" variant='flushed' colorScheme='none' />
+                  {/* <InputRightElement width='4.5rem'>
+                    <IconButton
+                      colorScheme='none'
+                      onClick={handleClick}
+                      icon={show ? <ViewOffIcon color={theme ? "white" : "black"} /> : <ViewIcon color={theme ? "white" : "black"} />}
+                    />
+                  </InputRightElement> */}
+                </InputGroup>
+              </FormControl>
+
+            </Stack>
+            <Box width='50vh' m='auto'>
+
+            </Box>
+            <Box color='gray.300'>
+
+              <Checkbox onChange={() => setChkBox((prev) => !prev)} colorScheme='blue' mt='25px'><Text fontSize='14px' name="termsAccepted" color={theme ? 'white' : 'blackAlpha.700'}>You accept our Terms and Privacy</Text></Checkbox>
+
+
+            </Box>
+
+            <Text color={'red'} textAlign={'center'} pt={"15px"} fontSize={'17px'}>{errMsg}</Text>
+
+            <Center>
+              <Button bg='#3DC6EF' width={{ base: '90%', md: '70%', lg: '80%' }} h='50px' mt='15' fontSize='18px' colorScheme='none'
+                color={theme ? 'white' : 'blackAlpha.800'}
+                _hover={{ bg: '#74d4f0' }}
+                onClick={handleSignUp}
+                isDisabled={subBtnDisable || !chkBox}
+              > Sign up</Button>
+            </Center>
+
+            <Center mt='30px'  >
+              <Button bg='#4086f3' width={{ base: '90%', md: '70%', lg: '80%' }} h='50px' fontSize='18px' color='white' colorScheme='none'>
+                <HStack bg='white' py='9px' px='10px' ml={{ base: '-12px', md: '-30px', lg: '-46px' }} borderRadius='5px 0 0 5px' ><FcGoogle size='30px' /></HStack>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Sign up with Google
+              </Button>
+            </Center>
 
           </Box>
-          <Box color='gray.300'>
-
-            <Checkbox colorScheme='blue' mt='25px'><Text fontSize='14px' name="termsAccepted" onChange={handleInput} color={theme ? 'white' : 'blackAlpha.700'}>You accept our Terms and Privacy</Text></Checkbox>
-            {/* <Box fontSize='14px' mt='10px'>By loggingin, you accept out Terms of use,</Box>
-            <Box fontSize='14px'> and Privacy & Cookies Statement.</Box> */}
-
-          </Box>
-          <Box>
-
-            <Button bg='#3DC6EF' width='85%' h='50px' mt='25' fontSize='18px' colorScheme='none'
-              // isDisabled={!validateState(state)}
-              className={!validateState(state) ? "Disabled" : null}
-              onClick={handleClick}
-              color={theme ? 'white' : 'blackAlpha.800'}
-              _hover={{bg:'#74d4f0'}}
-            > Sign up</Button>
-{/* h='50px' width='100px' */}
-          </Box>
-          <Center mt='20px'  >
-            <Button bg='#4086f3' width='85%' h='50px' fontSize='18px' color='white' colorScheme='none'>
-              <HStack bg='white' py='8.5px'  px='10px' ml='-5px' borderRadius='5px 0 0 5px' ><FcGoogle size='30px'/></HStack>
-              &nbsp;&nbsp; Sign up with Google &nbsp;&nbsp;
-            </Button>
-          </Center>
-
-
-        </Box>
-      </Center>
-    </Box>
-   
+        </Center>
+      </Box>
     </>
-
-
-
   )
 }
 
